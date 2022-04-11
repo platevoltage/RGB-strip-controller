bb99c83492eeec923372bb862fef3f59b116d81d
antieatingactivist <antieatingactivist@gmail.com>
2022-04-11 13:47:26 -0700

    color picker now sets its own state when changed

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { PhotoshopPicker } from 'react-color';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState("#ffffff");
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <PhotoshopPicker color={ state } onChangeComplete={ (color) => setState(color.hex) }/>
     </div>
   );
 }
