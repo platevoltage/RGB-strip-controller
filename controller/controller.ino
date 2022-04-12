09caaf8aae98a40a9db6cfd4e8b3b17677b7ca9b
antieatingactivist <antieatingactivist@gmail.com>
2022-04-12 12:49:18 -0700

    react interface can now change colors of leds

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,6 +5,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
 
 
 
@@ -13,13 +14,18 @@
 #define STAPSK  "youcanttaketheskyfromme"
 #endif
 
+
+const int dataPin = 15;       //ws2801 data pin
+
+const char* bonjourName = "RGB-Strip";  //bonjour name - http://xxxx.local 
+
 const char *ssid = STASSID;
 const char *password = STAPSK;
-uint stripLength = 20;
+uint stripLength = 40;
 uint currentData[100][4] = {};
 // uint currentData[2] = {255, 233};
 
-
+Adafruit_NeoPixel pixels(stripLength, dataPin, NEO_GRB + NEO_KHZ800);
 ESP8266WebServer server(80);
 
 
@@ -128,7 +134,20 @@ void updateConfig() {
       currentData[position][3] = white;
 
       
+
+      
     }
+
+    for (int i = 0; i < stripLength; i++) {
+      byte red = currentData[i][0];
+      byte green = currentData[i][1];
+      byte blue = currentData[i][2];
+      
+
+      pixels.setPixelColor(i, Color(red, green, blue)); 
+      pixels.show();   
+    }
+
     
   }
   
@@ -143,6 +162,7 @@ void setup(void) {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
+  pixels.begin();
 
 
 
@@ -158,7 +178,7 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("RGB-controller")) {
+  if (MDNS.begin(bonjourName)) {
     Serial.println("MDNS responder started");
   }
 
@@ -172,10 +192,23 @@ void setup(void) {
   server.begin();
 
   Serial.println("HTTP server started");
+  
 }
+uint32_t Color(byte r, byte g, byte b) {
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+
 
 void loop(void) {
   server.handleClient();
   MDNS.update();
+          
 }
 
