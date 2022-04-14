4afb4a5440a158ee2f42f4b27571181aec82407a
antieatingactivist <antieatingactivist@gmail.com>
2022-04-14 01:00:48 -0700

    implimented white level slider

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,23 +1,48 @@
 
 // import './App.css';
-import { HuePicker, AlphaPicker } from 'react-color';
+import { HuePicker, AlphaPicker, SliderPicker } from 'react-color';
+import { Hue, Alpha } from 'react-color/lib/components/common';
 import { useState } from 'react';
 import CurrentConfig from './components/CurrentConfig';
-
+import { RGBToHSL, HSLtoRGB } from './utils/conversion';
 
 function App() {
   const [pickerColor, setPickerColor] = useState({ r: 0, g: 0, b: 0 });
   const [whiteLevel, setWhiteLevel] = useState({ r: 0, g: 0, b: 0, a: 0 });
   
+  const whiteSlider = {...whiteLevel, a: 1-whiteLevel.a};
+
+  console.log(whiteLevel);
+  console.log(whiteSlider);
   const style = {
     marginTop: "100px"
   }
-
+  // console.log(pickerColor);
   return (
     <div className="App" style={style}>
-
-      <HuePicker color={ pickerColor } onChange={ (color) => setPickerColor({ r: color.rgb.r, g: color.rgb.g, b: color.rgb.b}) }/>
-      <AlphaPicker color={ whiteLevel } onChange={ (color) => setWhiteLevel({ r: 0, g: 0, b: 0, a: color.rgb.a }) }/>
+      <div style={{margin: "30px"}}>
+        {/* <div>
+          Hue
+          <SliderPicker color={ pickerColor } onChange={ (color) => setPickerColor({ r: color.rgb.r, g: color.rgb.g, b: color.rgb.b}) }/>
+        </div>
+        <div style={{marginTop: "20px"}}>
+          White Level
+          <AlphaPicker color={ whiteLevel } onChange={ (color) => setWhiteLevel({ r: 0, g: 0, b: 0, a: color.rgb.a }) }/>
+        </div> */}
+        <div style={{marginTop: "20px"}}>
+          Hue
+          <div style={{height: "20px", width: "1000px", position: "relative"}}>
+            <Hue hsl={ RGBToHSL(pickerColor) }  onChange={ (color) => setPickerColor(HSLtoRGB(color)) } />
+          </div>
+          White Level
+          <div style={{height: "20px", width: "1000px", position: "relative", backgroundColor: "#ffffff", transform: "scale(-1, 1)"}}>
+            <Alpha rgb={ whiteSlider } hsl={ {h:0,s:0,l:0} } onChange={ (color) => setWhiteLevel({ r: 0, g: 0, b: 0, a: color.a }) } />
+          </div>
+          
+        </div>
+        
+      </div>
+      
 
 
      <CurrentConfig pickerColor={ pickerColor } whiteLevel={ whiteLevel.a*255 }/>
--- color-picker/src/utils/conversion.js
+++ color-picker/src/utils/conversion.js
@@ -34,3 +34,87 @@ export const compare = (oldData, newData) => {
     }
     return {changes, changePositions};
 }
+
+
+export const RGBToHSL = ({r, g, b}) => {
+    // Make r, g, and b fractions of 1
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+  
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r,g,b);
+    let cmax = Math.max(r,g,b);
+    let delta = cmax - cmin;
+    let h = 0;
+    let s = 0;
+    let l = 0;
+    if (delta === 0)
+    h = 0;
+  // Red is max
+    else if (cmax === r)
+        h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax === g)
+        h = (b - r) / delta + 2;
+    // Blue is max
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+        
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+          
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    
+
+
+    return {h, s, l};
+
+  }
+
+
+  export const HSLtoRGB = ({h, s, l}) => {
+    // Must be fractions of 1
+    // console.log(h, s, l);
+    // s /= 100;
+    s = 1;
+    // l /= 100;
+    l = .5;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0,
+        b = 0;
+
+        if (0 <= h && h < 60) {
+            r = c; g = x; b = 0;  
+          } else if (60 <= h && h < 120) {
+            r = x; g = c; b = 0;
+          } else if (120 <= h && h < 180) {
+            r = 0; g = c; b = x;
+          } else if (180 <= h && h < 240) {
+            r = 0; g = x; b = c;
+          } else if (240 <= h && h < 300) {
+            r = x; g = 0; b = c;
+          } else if (300 <= h && h < 360) {
+            r = c; g = 0; b = x;
+          }
+          r = Math.round((r + m) * 255);
+          g = Math.round((g + m) * 255);
+          b = Math.round((b + m) * 255);
+        
+        // console.log(r, g, b);
+    return {r, g, b};
+  }
