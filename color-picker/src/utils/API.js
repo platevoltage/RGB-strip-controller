0d450ea9981f21fd0dbc17e14550bf5b8eb8ddf8
antieatingactivist <antieatingactivist@gmail.com>
2023-01-07 13:43:34 -0800

    updated frontend payload

--- color-picker/src/utils/API.js
+++ color-picker/src/utils/API.js
@@ -10,21 +10,12 @@ export const getCurrentConfig = (address) => {
 
 export const writeChanges = (stripLength, oldData, newData, address, dividers, effectSpeed) => {
     const data = compare(oldData, newData);
-    // const redArray = [];
-    // const greenArray = [];
-    // const blueArray = [];
-    // const whiteArray = [];
     const colorArray = [];
 
     for (let i of data.changes) {
         let rgbwData = i;
-        // redArray.push(rgbwData.r);
-        // greenArray.push(rgbwData.g);
-        // blueArray.push(rgbwData.b);
-        // whiteArray.push(rgbwData.w);
         colorArray.push(((rgbwData.w << 24) | (rgbwData.r << 16) | (rgbwData.g << 8) | rgbwData.b) >>> 0);
     }
-    // console.log([...dividers.sort((x, y) => { return  x - y }).filter(x => {return x!==0 }), 0, 0, 0, 0]);
     return fetch(`${address}/update`, {
         method: 'POST',
         headers: {
@@ -37,10 +28,6 @@ export const writeChanges = (stripLength, oldData, newData, address, dividers, e
             "dividers": [...dividers.sort((x, y) => { return  x - y }).filter(x => {return x!==0 }), 0, 0, 0, 0],
             "length" : data.changes.length,
             "positions" : data.changePositions,
-            // "red" : redArray,
-            // "green" : greenArray,
-            // "blue" : blueArray,
-            // "white" : whiteArray,
             "color" : colorArray
             
         })
