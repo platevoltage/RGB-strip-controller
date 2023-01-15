fd662dc56f75ac7e5aa5cd83a42933f00e1de674
antieatingactivist <antieatingactivist@gmail.com>
2023-01-14 21:50:55 -0800

    time on schedule updates correctly

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -99,6 +99,7 @@ function App() {
       <ColorLayout set={globalSetters} get={globalGetters}/>
 
       <Schedule schedule={schedule} setSchedule={setSchedule} colors={colors} currentTime={currentTime}/>
+      
       <CurrentConfig set={globalSetters} get={globalGetters}/>
  
      
