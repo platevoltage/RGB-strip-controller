c1d15b2cd64bcc4a65e29f6c04caf123af9ab6fc
antieatingactivist <antieatingactivist@gmail.com>
2023-01-17 19:11:18 -0800

    created preference panel

---
+++ color-picker/src/components/PreferencePanel.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function PreferencePanel() {
+
+    const style = {
+        backgroundColor: '#555555ee',
+        width: '500px',
+        height: "400px",
+        backdropFilter: "blur(8px)",
+        webkitBackdropFilter: "blur(8px)",
+        mozBackdropFilter: "blur(8px)",
+        border: '1px solid #ffffff33',
+        borderRadius: '3px'
+
+    }
+    return (
+        <div style={style}>
+        
+        </div>
+    )
+}
