94c4bf9e6689588eeb5ae97836e3573f155f0020
antieatingactivist <antieatingactivist@gmail.com>
2023-01-23 19:00:03 -0800

    added env

--- controller/src/payload/static/css/main.c83abc47.css.h
+++
@@ -1,3 +0,0 @@
-static const char _main_css[] PROGMEM = R"=====(
-body{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;background-color:#000;color:#fff;display:flex;font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;justify-content:center;margin:0;-webkit-user-select:none;user-select:none}code{font-family:source-code-pro,Menlo,Monaco,Consolas,Courier New,monospace}
-)=====";
